dbb19da6f9a6374b9edde58ab3ac790e455705c4
Mona Lisa <isaac11195@gmail.com>
2021-07-15 10:25:21 -0400

    add new commands

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -2,6 +2,8 @@ import React, {useEffect} from 'react';
 import { useQuery } from '@apollo/client';
 
 import ProductItem from '../ProductItem';
+
+import { idbPromise } from "../../utils/helpers";
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from "../../utils/actions";
 import { QUERY_PRODUCTS } from '../../utils/queries';
@@ -19,13 +21,27 @@ const { loading, data } = useQuery(QUERY_PRODUCTS);
   const products = data?.products || [];
 
   useEffect(() => {
-    if (data) {
+    if(data) {
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+  
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+      // add else if to check if `loading` is undefined in `useQuery()` Hook
+    } else if (!loading) {
+      // since we're offline, get all of the data from the `products` store
+      idbPromise('products', 'get').then((products) => {
+        // use retrieved data to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
+        });
+      });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
   
   function filterProducts() {
     if (!currentCategory) {
