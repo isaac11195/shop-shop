065a128cffd31391bb1460a852c80dc08e7f088f
Mona Lisa <isaac11195@gmail.com>
2021-07-19 19:31:13 -0400

    add new command

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,14 +1,29 @@
 import React, { useEffect } from "react";
 import CartItem from "../CartItem";
 import Auth from "../../utils/auth";
+import { useLazyQuery } from '@apollo/client';
 import { idbPromise } from "../../utils/helpers";
 import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
 import "./style.css";
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
+
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
+
+  
   useEffect(() => {
     async function getCart() {
       const cart = await idbPromise('cart', 'get');
@@ -38,6 +53,19 @@ const Cart = () => {
     return sum.toFixed(2);
   }
 
+  function submitCheckout() {
+    const productIds = [];
+  
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
+    getCheckout({
+      variables: { products: productIds }
+    });
+  }
+
   if (!state.cartOpen) {
     return (
       <div className="cart-closed" onClick={toggleCart}>
@@ -48,7 +76,6 @@ const Cart = () => {
     );
   }
 
-  
   return (
     <div className="cart">
       <div className="close" onClick={toggleCart}>[close]</div>
@@ -64,9 +91,9 @@ const Cart = () => {
 
             {
               Auth.loggedIn() ?
-                <button>
-                  Checkout
-              </button>
+              <button onClick={submitCheckout}>
+              Checkout
+            </button>
                 :
                 <span>(log in to check out)</span>
             }
